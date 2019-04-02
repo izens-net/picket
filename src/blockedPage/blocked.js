420bc34f5ed5b23698e608a93ec2278832156585
James Henri Haydon <james.haydon@gmail.com>
2019-04-02 21:38:45 +0100

    Add name and link of union in blocked page.

--- src/blockedPage/blocked.js
+++ src/blockedPage/blocked.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const message = new URL(document.URL).searchParams.get("msg");
-  const union = new URL(document.URL).searchParams.get("union");
+  const params = new URL(document.URL).searchParams;
+  const message = params.get("msg");
+  const union = params.get("union");
+  const union_url = params.get("union_url");
+  Array.forEach(document.querySelectorAll(".union-name"), (el,i) => { el.innerText = union; });
+  document.getElementById("union-link").href = union_url;
   document.getElementById("loaded-message").innerText = message;
   document.getElementById("reason").innerText
     = "The reason provided by " + union + " is:"
