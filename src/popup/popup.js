11ab754b4f8cf55d243dee16b7c96a105afc1d95
Anike Arni <anikarni@gmail.com>
2019-07-08 20:53:31 +0200

    add campaign name to popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,18 +1,34 @@
 import loadPolicy from '../loadPolicy'
 
+const showSuccessMessage = () => {
+  var animateIn = document.getElementById("notification")
+  animateIn.className = "show"
+  setTimeout(function(){
+    animateIn.className = animateIn.className.replace("show", "")
+  }, 3000)
+}
+
+const showJoinedCampaigns = () => {
+  chrome.storage.sync.get(['policy'], (value) => {
+    if (value.policy) {
+      const campaignsEl = document.getElementById('campaigns')
+      campaignsEl.innerText = `Your campaign: \n${value.policy.name}`
+      campaignsEl.className = "campaigns"
+    }
+  })
+}
+
+const submitForm = (ev) => {
+  ev.preventDefault()
+  const policyFileUrl = new FormData(policyFileForm).get('policyFile')
+  loadPolicy(policyFileUrl)
+    .then(showSuccessMessage)
+    .then(showJoinedCampaigns)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const policyFileForm = document.getElementById('policyFileForm')
-  policyFileForm.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    const policyFileUrl = new FormData(policyFileForm).get('policyFile')
-    loadPolicy(policyFileUrl)
-      .then(e => {
-        var animateIn = document.getElementById("notification");
-        animateIn.className = "show";
-        setTimeout(function(){
-          animateIn.className = animateIn.className.replace("show", "");
-        }, 3000);
-        e.preventDefault();
-      })
-  })
+
+  policyFileForm.addEventListener('submit', submitForm)
+  showJoinedCampaigns()
 })
